ced62a43225a7e754c6c80734fdfab4a8fdc8769
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-01-30 02:46:09 +0200

    Update tp-client_exports.lua

--- tpz_leveling/client/tp-client_exports.lua
+++ tpz_leveling/client/tp-client_exports.lua
@@ -2,7 +2,7 @@
 
 
 -- @param actionType : The required type to retrieve the data such as level and experience.
--- Example: local level, experience = tpz_leveling:GetLevelTypeExperienceData('mining')
+-- Example: local level, experience = exports.tpz_leveling:GetLevelTypeExperienceData('mining')
 exports('GetLevelTypeExperienceData', function(actionType)
     return GetLevelTypeExperienceData(actionType)
 end)
