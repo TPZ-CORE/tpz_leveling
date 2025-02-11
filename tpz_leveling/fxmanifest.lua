dd774e2f3a53c52c92e92a45e706483cec19d9fc
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-11 22:36:04 +0200

    Update fxmanifest.lua

--- tpz_leveling/fxmanifest.lua
+++ tpz_leveling/fxmanifest.lua
@@ -8,7 +8,7 @@ version '1.0.0'
 
 server_scripts { 'server/*.lua' }
 client_scripts { 'client/*.lua' }
-shared_scripts { 'config.lua }
+shared_scripts { 'config.lua' }
 
 dependencies {
     'tpz_core',
