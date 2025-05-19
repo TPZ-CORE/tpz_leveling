c12688493b31193578ed45ed024036c6fa2d475a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-19 19:38:11 +0300

    Update tp-server_main.lua

--- tpz_leveling/server/tp-server_main.lua
+++ tpz_leveling/server/tp-server_main.lua
@@ -5,7 +5,7 @@ local TPZ = exports.tpz_core:getCoreAPI()
 -----------------------------------------------------------
 
 -- @GetTableLength returns the length of a table.
-local function GetTableLength(T)
+function GetTableLength(T)
     local count = 0
     for _ in pairs(T) do count = count + 1 end
     return count
