3245cbe538d8f5ca5df991fe21cf6c1ff1b67c51
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-01-07 17:07:00 +0200

    Update fxmanifest.lua

--- tpz_leveling/fxmanifest.lua
+++ tpz_leveling/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE Leveling'
-version '1.0.0'
+version '1.0.1'
 
 server_scripts { 'server/*.lua' }
 client_scripts { 'client/*.lua' }
